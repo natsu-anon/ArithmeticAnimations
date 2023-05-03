92ce136d539e0d04e9d048f3f962b17e35bb76c2
natsu-anon <jarano93@gmail.com>
2023-05-03 15:33:39 -0700

    yeah just delete the dir after zipping ezpz

--- prep.ps1
+++ prep.ps1
@@ -4,4 +4,5 @@ cp .\arithmetic_animations.py Arithmetic_Animations\arithmetic_animations.py
 cp .\ui.py Arithmetic_Animations\ui.py
 cp .\__init__.py Arithmetic_Animations\__init__.py
 zip Arithmetic_Animations Arithmetic_Animations\*
+rm .\Arithmetic_Animations\*
 echo "Ready!"
